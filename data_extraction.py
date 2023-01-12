6ed307fa29edf6a7fcb49e93be88d01f1a635ea8
Mike Maruthan <mikemaruthan@Mikes-MacBook-Pro.local>
2023-01-12 15:37:34 +0000

    Creating a data_extraction python script

---
+++ data_extraction.py
@@ -0,0 +1,7 @@
+
+class DataExtractor:
+    '''
+    This class will work as a utility class, in this class, methods will be created that will help extract data from different data sources.
+    The methods conatined will be fit to extract data from a particular data source, including CSV files, an API and an S3 bucket.
+    '''
+    
