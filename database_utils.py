8670c4f65560cc017e9f6b74c3dc08b83460b474
Mike Maruthan <mikemaruthan@Mikes-MacBook-Pro.local>
2023-02-20 08:24:10 +0000

    Creating the read_db_creds method to read the credentials yaml file

--- database_utils.py
+++ database_utils.py
@@ -1,5 +1,13 @@
+import yaml 
 
 class DatabaseConnector:
     '''
     This class will be used to connect and upload the extracted data to the database.
     '''
+
+    # the read_db_creds() method will read the credentials yaml file and return the dictionary of the credentials
+    def read_db_creds(self):
+        with open('db_creds.yaml') as f:
+            self.credentials = yaml.safe_load(f)
+
+    
