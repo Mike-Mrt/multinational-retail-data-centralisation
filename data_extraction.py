fc548f02478f464d5e488dfc9b51f6dfeae969bf
Mike Maruthan <mikemaruthan@Mikes-MacBook-Pro.local>
2023-02-20 07:47:23 +0000

    Creating a yaml file containing credentials for the database

--- data_extraction.py
+++ data_extraction.py
@@ -2,6 +2,5 @@
 class DataExtractor:
     '''
     This class will work as a utility class, in this class, methods will be created that will help extract data from different data sources.
-    The methods conatined will be fit to extract data from a particular data source, including CSV files, an API and an S3 bucket.
+    The methods contained will be fit to extract data from a particular data source, including CSV files, an API and an S3 bucket.
     '''
-    
