de6393a7b9ea08013c1d5bc73fb9a660f67037fd
Mike Maruthan <mikemaruthan@Mikes-MacBook-Pro.local>
2023-02-26 15:13:07 +0000

    Creating a retrieve_pdf_data method which will take in a url for a pdf and return a pandas dataframe

--- data_extraction.py
+++ data_extraction.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import sqlalchemy
+import tabula
 import database_utils
 
 # Had to downgradre to SQLAlchemy version 1.4.46 in order to not attain the following error: AttributeError: 'OptionEngine' object has no attribute 'execute'
@@ -16,9 +16,15 @@ class DataExtractor:
         users = pd.read_sql_table(table_name,engine)
         return users
 
-testing = DataExtractor()
-db_connector = database_utils.DatabaseConnector()
-db_connector.read_db_creds()
-db_connector.init_db_creds()
-users = testing.read_rds_table('legacy_users', db_connector)
-# print(users.head())
+    # The retrieve_pdf_data method will take in a link as an argument and return a pandas dataframe:
+    def retrieve_pdf_data(self, pdf_url):
+        # Tabula package reads the pdf and we want all pages read, since it splits each page into different dfs we want to ensure all pages of the pdf are read and returned as a list of dataframes
+        self.df_cards_list = tabula.read_pdf(pdf_url, pages='all', multiple_tables=True)
+        # We then return a single pd dataframe which concatenates all the lists of data frames from above:  
+        return pd.concat(self.df_cards_list)
+
+# testing = DataExtractor()
+
+# cards_df = testing.retrieve_pdf_data('https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf')
+# print(cards_df.head())
+
